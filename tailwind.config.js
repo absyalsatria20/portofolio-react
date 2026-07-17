b49e846cc1bbc233e6022af9667ac26b6fa686ed
absyalsatria20 <absyalsatria20@gmail.com>
2026-07-17 16:04:57 +0700

    Undo perubahan

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // INI KUNCI UTAMANYA!
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Memasukkan font Outfit sebagai font utama (sans)
+      fontFamily: {
+        sans: ['Outfit', 'sans-serif'],
+      },
+      // Warna identitasmu sekalian kita pastikan sama persis
+      colors: {
+        dark: '#0a0a0a',
+        darker: '#050505',
+        accent: '#6366f1',
+        accentHover: '#4f46e5'
+      }
+    },
   },
   plugins: [],
 }
